d03c34978068022f7d0d6c776400151c1ddd773a
lu <pengluustc@gmail.com>
2015-05-06 10:39:25 -0700

    use custom tag to add school name to all url on school page

--- mediaWatch_lu/urls.py
+++ mediaWatch_lu/urls.py
@@ -11,17 +11,18 @@ urlpatterns = [
     url(r'^schools/page=(?P<page>\d+)/$', 'schools.views.list_view', name = 'page_list'),
 #     url(r'^schools/state=(?P<state>\w+)/rank_range=(?P<rank_range>\w+)/page=(?P<page>\d+)/$', 
 #         'schools.views.list_view', name = 'selected_list'),
+    
+    url(r'^schools/school_id=(?P<school_id>\d+)/.+/media/$', 
+        'schools.views.media_view', name = 'school_media'), # media details: search engine, site, language, social media
+    url(r'^schools/school_id=(?P<school_id>\d+)/.+/info/$', 
+        'schools.views.info_view', name = 'school_info'),
+    url(r'^schools/school_id=(?P<school_id>\d+)/.+/news/$', 
+        'schools.views.news_view', name = 'school_news'),
+    url(r'^schools/school_id=(?P<school_id>\d+)/.+/report/$', 
+        'schools.views.report_view', name = 'school_report'),
+    url(r'^schools/school_id=(?P<this_id>\d+)/.+/compare/$', 'schools.views.compare_view', name = 'school_comparison'),
     url(r'^schools/school_id=(?P<school_id>\d+)/.+/$', 
         'schools.views.school_view', name = 'school_page'),
-    url(r'^schools/school_id=(?P<school_id>\d+)/media/$', 
-        'schools.views.media_view', name = 'school_media'), # media details: search engine, site, language, social media
-    url(r'^schools/school_id=(?P<school_id>\d+)/info/$', 
-        'schools.views.info_view', name = 'school_info'),
-    url(r'^schools/school_id=(?P<school_id>\d+)/news/$', 
-        'schools.views.news_view', name = 'school_news'),
-    url(r'^schools/school_id=(?P<school_id>\d+)/report/$', 
-        'schools.views.report_view', name = 'school_report'),
-    url(r'^schools/school_id=(?P<this_id>\d+)/compare/$', 'schools.views.compare_view', name = 'school_comparison'),
     url(r'^schools/custom_selection/$', 'schools.views.custom_selection', name = 'custom_selection'), # custom selection of schools
     url(r'^schools/contact_us/$', 'schools.views.contact_view'),
 ]
