51c4eeaf313c9c8dfeaa82417e5c5d652be91d1b
lu <pengluustc@gmail.com>
2015-05-12 10:49:39 -0700

    move static file out of source and disable admin

--- mediaWatch_lu/settings.py
+++ mediaWatch_lu/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -110,8 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
-STATICFILES_DIRS = (
-                    os. path. join(BASE_DIR, 'schools' , 'static' ),
-)
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+#STATICFILES_DIRS = (
+#                    os. path. join(BASE_DIR, 'schools' , 'static' ),
+#)
 
--- mediaWatch_lu/urls.py
+++ mediaWatch_lu/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # url(r'^$', 'media_watch_education_lu.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
     url(r'^schools/$', 'schools.views.list_view', name = 'full_list'),
     url(r'^schools/page=(?P<page>\d+)/$', 'schools.views.list_view', name = 'page_list'),
 #     url(r'^schools/state=(?P<state>\w+)/rank_range=(?P<rank_range>\w+)/page=(?P<page>\d+)/$', 
