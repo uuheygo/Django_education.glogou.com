5f547716c247dec16b959ff6fd939ae635559970
lu <pengluustc@gmail.com>
2015-06-03 09:32:11 -0700

    remove schools in all url

--- mediaWatch_lu/urls.py
+++ mediaWatch_lu/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'^schools/', include('schools.urls')),
+    url(r'^', include('schools.urls')),
 ]
