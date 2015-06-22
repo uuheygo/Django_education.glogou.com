78e390f35e8d88ed3fdf3eb5bc4b77c018fc18c9
binlee <bin.lee@gmail.com>
2015-06-22 13:50:28 -0700

    Add url support for robots.txt. One main reason we need this is that we do not want search engine indexed our development server, we only want search engine index production server.  So that we can use robots.txt to control this.
    	modified:   mediaWatch_lu/urls.py

--- mediaWatch_lu/urls.py
+++ mediaWatch_lu/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView, TemplateView
 
 urlpatterns = [
     # Examples:
@@ -8,4 +9,9 @@ urlpatterns = [
 
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('schools.urls')),
+
+    # Add this line for robots.txt, one main reason is that, we do not want search engine to index
+    # our testing server. NOTE, we will not check-in robots.txt to github, because
+    # production server and testing server will have different robots.txt file.
+    url(r'^robots\.txt$', TemplateView.as_view(template_name="robots.txt",  content_type='text/plain')),
 ]
